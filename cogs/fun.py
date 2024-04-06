a3f1087b0cd9dfa03d453483b6f85473fa5b7e7b
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-06 16:37:19 -0700

    more cowsay shit

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,8 +12,8 @@ class Fun(commands.Cog):
         self.bot = bot
         self._last_member = None
     @commands.slash_command(name="cowsay", description="The cowsay command from Linux!")
-    async def cowsay(self, ctx, text=discord.Option(str, description="Text for cow to say", required="true")):
-        await ctx.respond(f"``` \n" + cowsay.get_output_string(f'cow', str(text)) + "\n ```")
+    async def cowsay(self, ctx, text=discord.Option(str, description="Text for cow to say", required="true"), character=discord.Option(str, description="Character for cowsay to use. The character list is: beavis, cheese, cow, daemon, dragon, fox, ghostbusters, kitty, meow, miki, milk, octopus, pig, stegosaurus, stimpy, trex, turkey, turtle, tux", required="true"):
+        await ctx.respond(f"``` \n" + cowsay.get_output_string(str(character), str(text)) + "\n ```")
 
 
 
