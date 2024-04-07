db2562922944ff1b4ed48a4e549d97513fad409c
Damien Boisvert <damien@alphagame.dev>
2024-04-07 06:43:36 +0000

    Update DISC2MBTI command to be overall better

--- cogs/disc2mbti.py
+++ cogs/disc2mbti.py
@@ -6,32 +6,53 @@ class Disc2mbti(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
+
+        self._MBTI_STRINGS = {
+            # index 0: I/E
+            "I": "Introverted",
+            "E": "Extraverted",
+            # index 1: N/S
+            "N": "Intuitive",
+            "S": "Sensing",
+            # index 2: T/F
+            "T": "Thinking",
+            "F": "Feeling",
+            # index 3: J/P
+            "P": "Prospecting",
+            "J": "Judging"
+        }
+        self._CONSTRUCT = "{0} *({1}, {2}, {3}, and {4})*"
+
     @commands.slash_command(title="disc2mbti", description="Converts your Indigo DISC scores into MBTI personality types!")
     async def disc2mbti(self, ctx, d: discord.Option(int, description="Your Dominance Score"), i: discord.Option(int, description="Your Influence Score"), s: discord.Option(int, description="Your Steadiness Score"), c: discord.Option(int, description="Your Compliance Score")):
         await ctx.respond("Your MBTI is:")
         
+        currentMbti = [None, None, None, None] # initalize empti mbti
         if i >= 50:
-            await ctx.send("E")
+            currentMbti[0] = "E"
         else:
-            await ctx.send("I")
+            currentMbti[0] = "I"
         
         if c >= 50:
-            await ctx.send("S")
+            currentMbti[1] = "S"
         else:
-            await ctx.send("N")
+            currentMbti[1] = "N"
         
         if d >= 50:
-            await ctx.send("T")
+            currentMbti[2] = "T"
         else:
-            await ctx.send("F")
+            currentMbti[2] = "F"
         
         if s >= 50:
-            await ctx.send("J")
+            currentMbti[3] = "J"
         else:
-            await ctx.send("P")
+            currentMbti[3] = "P"
 
-
-
+        await ctx.respond(self._CONSTRUCT.format("".join(currentMbti),
+                                                 self._MBTI_STRINGS[currentMbti[0]],
+                                                 self._MBTI_STRINGS[currentMbti[1]],
+                                                 self._MBTI_STRINGS[currentMbti[2]],
+                                                 self._MBTI_STRINGS[currentMbti[3]]))
 
 
 
