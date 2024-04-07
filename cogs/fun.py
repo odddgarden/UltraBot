60e3565c074ccc46d3f90036ee4cbe290da85cc4
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-06 19:55:51 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,9 +14,9 @@ class Fun(commands.Cog):
 
     @commands.slash_command(name="cowsay", description="The cowsay command from Linux!")
     async def cowsay(self, ctx, text=discord.Option(str, description="Text for cow to say", required=True), 
-                     character=discord.Option(str, description="Character for cowsay to use.", 
+                     character=discord.Option(str, description="Character for cowsay to use."), 
                      choices=cowsay.char_names, default="cow"):
-        await ctx.respond("```\n{0}\n```".format(cowsay.get_output_string(character, text))
+        await ctx.respond("```\n{0}\n```".format(cowsay.get_output_string(character, text))),
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Fun(bot)) # add the cog to the bot
