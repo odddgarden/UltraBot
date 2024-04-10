89168711e2fb795e4669d109cdc14813c60cdd40
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-09 21:12:05 -0700

    updated ersion and message

--- main.py
+++ main.py
@@ -86,12 +86,12 @@ async def helloworld(ctx):
 @bot.slash_command(name="about", description="About the bot")
 async def about(ctx):
     embed = discord.Embed(
-        title= "About UltraBot 1.0.1",
+        title= "About UltraBot 1.0.2",
         description= "UltraBot is a Python based discord bot created by CombineSoldier14 with commands for moderation and fun!\n UltraBot's birthday is **4/5/2024.**",
         color=discord.Colour.yellow(),
     )
     embed.set_thumbnail(url="https://camo.githubusercontent.com/7ebe7e305bde0efefd93829ed13a016cbfcad30985449dd5d54f612174aceb44/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6170702d69636f6e732f313232353232303736343836313733303836372f66363662643462656234663165626565303638356438633563666436343662622e706e673f73697a653d323536")
-    embed.add_field(name="**Latest Addition**", value="Added /dogpics!")
+    embed.add_field(name="**Latest Addition**", value="Added /avatar!")
     await ctx.respond(embed=embed, view=AboutLinkBloggerView())
 
 
