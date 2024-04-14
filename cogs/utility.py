95dc1d6725ad8b7b5032ef411314c919c7ee66cf
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-14 14:19:42 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -62,22 +62,21 @@ class utility(commands.Cog):
         await ctx.respond("Time has been resumed!")
     
     @commands.slash_command(name="userinfo", description="Gets info on a user in the server!")
-    async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True))
-      embed = discord.Embed(
+    async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True)):
+        embed = discord.Embed(
             title="Info on {0}".format(user),
             description="""
-            **ID:** user.id
-            **Joined Discord:** user.created_at
-            **Roles:** user.roles
-            **Discriminator:** user.discriminator
-            **Is A Bot?:** user.bot
-            """,
+            **ID:** {0}
+            **Joined Discord:** {1}
+            **Discriminator:** {2} 
+            **Is A Bot?:** {3} 
+            """.format(user.id, user.created_at, user.discriminator, user.bot),
             color=user.color,
           
-          )
-          embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-          embed.set_thumbnail(url=user.avatar)
-          await ctx.respond(embed=embed)
+        )
+        embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+        embed.set_thumbnail(url=user.avatar)
+        await ctx.respond(embed=embed)
           
 
     @commands.slash_command(name="botinfo", description="Info about UltraBot!")
