441c7008c146b30d6bfb2b1f53eae2acdec02691
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-15 19:37:47 -0700

    Update disc2mbti.py

--- cogs/disc2mbti.py
+++ cogs/disc2mbti.py
@@ -1,116 +1,54 @@
 import discord
 from discord.ext import commands
 import os
-import time
-import json
-
-with open("version.json", "r") as f:
-            _r = json.load(f)
-            VERSION = _r["VERSION"]
 
 
-class utility(commands.Cog):
-    group = discord.SlashCommandGroup(name="utility", description="Useful utility commands")
+
+
+class Disc2mbti(commands.Cog):
+    group = discord.SlashCommandGroup(name="fun", description="Non-serious commands for fun and enjoyment")
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
-    
-    @group.command(name="avatar", description="Find the avatar of the mentioned user!")
-    async def avatar(self, ctx, user: discord.Option(discord.Member, description="Member to get avatar of", required=True)):
-      await ctx.respond(user.display_avatar)
-
-
-    @group.command(name="makeembed", description="Make your own embed!")
-    async def makeembed(self, ctx, title: discord.Option(str, description="Title of embed"), description: discord.Option(str, description="Description of embed"), footer: discord.Option(str, description="Footer of embed"), color: discord.Option(int, description="Color of embed in hex format")):
-        embed = discord.Embed(
-            title=title,
-            description=description,
-            color=color,
-        )
-        embed.set_footer(text=footer)
-        await ctx.respond(embed=embed)
-
-    
-    @group.command(name="gettime", description="Returns the current date and time.")
-    async def gettime(self, ctx):
-        await ctx.respond(time.ctime)
-
-    @group.command(name="timestop", description="Stop time in a server JJBA style")
-    @commands.has_permissions(manage_channels=True)
-    async def timestop(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        embed = discord.Embed(
-            title="ZA WARUDO!",
-            description="Time has been stopped! No messages can be sent except for admins.",
-            color=discord.Colour.red(),
-        )
-        embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-        embed.set_image(url="https://i.redd.it/05vtn9chak101.gif")
-        await ctx.respond(embed=embed)
-
-    @group.command(name="resume", description="Resumes time in a server")
-    @commands.has_permissions(manage_channels=True)
-    async def resume(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-        await ctx.respond("Time has been resumed!")
-    
-    @group.command(name="userinfo", description="Gets info on a user in the server!")
-    async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True)):
-        if user.bot == True:
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
-             **ID:** {0}
-             **Joined Discord:** {1}
-             **Discriminator:** {2} 
-             **Is A Bot?:** {3} 
-             """.format(user.id, user.created_at, user.discriminator, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url=user.avatar)
         
-        if user.bot == False:
-    
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
-             **ID:** {0}
-             **Joined Discord:** {1} 
-             **Is A Bot?:** {2} 
-             """.format(user.id, user.created_at, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url=user.avatar)
-        await ctx.respond(embed=embed)
-          
 
-    @group.command(name="botinfo", description="Info about UltraBot!")
-    async def botinfo(self, ctx):
-         embed = discord.Embed(
-              title="Bot Info",
-              description="""
-              **Bot Name:** UltraBot
-              **Bot Owner:** @combinesoldier14
-              **Creation Date:** 4/5/2024
-              **Server Count:** Unavailable
-              **Library**: Py-cord {0}
-              """.format(discord.__version__),
-              color=discord.Colour.og_blurple(),
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         await ctx.respond(embed=embed)
+       
 
+    @group.command(title="disc2mbti", description="Converts your Indigo DISC scores into MBTI personality types!")
+    async def disc2mbti(self, ctx, d: discord.Option(int, description="Your Dominance Score"), i: discord.Option(int, description="Your Influence Score"), s: discord.Option(int, description="Your Steadiness Score"), c: discord.Option(int, description="Your Compliance Score")):
+        
+       
+        if i >= 50:
+            IE = "E"
+            longIE = "Extraverted"
+        else:
+            IE = "I"
+            longIE = "Introverted"
+        
+        if c >= 50:
+            SN = "S"
+            longSN = "Sensing"
+        else:
+            SN = "N"
+            longSN = "Intuitive"
+        
+        if d >= 50:
+            TF = "T"
+            longTF = "Thinking"
+        else:
+            TF = "F"
+            longTF = "Feeling"
+        
+        if s >= 50:
+            PJ = "J"
+            longPJ = "Judging"
+        else:
+            PJ = "P"
+            longPJ = "Perceiving"
 
-
-
-
-
+        await ctx.respond("Your MBTI is: {0}{1}{2}{3}".format(IE, SN, TF, PJ))
+        await ctx.send("_({0}, {1}, {2}, and {3})_".format(longIE, longSN, longTF, longPJ))
 
 
 def setup(bot): # this is called by Pycord to setup the cog
-    bot.add_cog(utility(bot)) # add the cog to the bot
+    bot.add_cog(Disc2mbti(bot)) # add the cog to the bot
