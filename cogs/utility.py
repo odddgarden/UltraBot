3acc293d9380f78840bbf6cc0641cc17fd620899
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-15 19:40:47 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -9,19 +9,18 @@ with open("version.json", "r") as f:
             VERSION = _r["VERSION"]
 
 
-
-
 class utility(commands.Cog):
+    group = discord.SlashCommandGroup(name="utility", description="Useful utility commands")
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
     
-    @commands.slash_command(name="avatar", description="Find the avatar of the mentioned user!")
+    @group.command(name="avatar", description="Find the avatar of the mentioned user!")
     async def avatar(self, ctx, user: discord.Option(discord.Member, description="Member to get avatar of", required=True)):
       await ctx.respond(user.display_avatar)
 
 
-    @commands.slash_command(name="makeembed", description="Make your own embed!")
+    @group.command(name="makeembed", description="Make your own embed!")
     async def makeembed(self, ctx, title: discord.Option(str, description="Title of embed"), description: discord.Option(str, description="Description of embed"), footer: discord.Option(str, description="Footer of embed"), color: discord.Option(int, description="Color of embed in hex format")):
         embed = discord.Embed(
             title=title,
@@ -31,18 +30,12 @@ class utility(commands.Cog):
         embed.set_footer(text=footer)
         await ctx.respond(embed=embed)
 
-    @commands.slash_command(name="thread", description="Create a new basic thread")
-    @commands.has_permissions(create_public_threads=True)
-    async def thread(self, ctx, title: discord.Option(str, description="Title of thread", required=True), startmsg: discord.Option(str, description="Starting message in thread", required=True)):
-        message = await ctx.send(startmsg)
-        await message.create_thread(name=title)
-        await ctx.respond("Your thread, **{0}**, has been created!".format(title))
-
-    @commands.slash_command(name="gettime", description="Returns the current date and time.")
+    
+    @group.command(name="gettime", description="Returns the current date and time.")
     async def gettime(self, ctx):
         await ctx.respond(time.ctime)
 
-    @commands.slash_command(name="timestop", description="Stop time in a server JJBA style")
+    @group.command(name="timestop", description="Stop time in a server JJBA style")
     @commands.has_permissions(manage_channels=True)
     async def timestop(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
@@ -55,13 +48,13 @@ class utility(commands.Cog):
         embed.set_image(url="https://i.redd.it/05vtn9chak101.gif")
         await ctx.respond(embed=embed)
 
-    @commands.slash_command(name="resume", description="Resumes time in a server")
+    @group.command(name="resume", description="Resumes time in a server")
     @commands.has_permissions(manage_channels=True)
     async def resume(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
         await ctx.respond("Time has been resumed!")
     
-    @commands.slash_command(name="userinfo", description="Gets info on a user in the server!")
+    @group.command(name="userinfo", description="Gets info on a user in the server!")
     async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True)):
         if user.bot == True:
          embed = discord.Embed(
@@ -95,7 +88,7 @@ class utility(commands.Cog):
         await ctx.respond(embed=embed)
           
 
-    @commands.slash_command(name="botinfo", description="Info about UltraBot!")
+    @group.command(name="botinfo", description="Info about UltraBot!")
     async def botinfo(self, ctx):
          embed = discord.Embed(
               title="Bot Info",
