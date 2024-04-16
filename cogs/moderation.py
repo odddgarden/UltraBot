f84a5e7bd692b892b92459f9fa2bce49be7cf914
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-15 19:39:07 -0700

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -10,18 +10,22 @@ with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
 
+
+
+
 class Moderation(commands.Cog):
+    group = discord.SlashCommandGroup(name="moderation", description="Commands for server management and moderation")
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
 
 
-    @commands.slash_command(name="userid", description="Find the ID of a mentioned user")
+    @group.command(name="userid", description="Find the ID of a mentioned user")
     async def userid(self, ctx, user: discord.Option(discord.Member, description="Member to find ID of", required="true")):
       await ctx.respond(f"That user's id is " + str(user.id))
 
-    @commands.slash_command(name="ban", description="Bans a user.")
+    @group.command(name="ban", description="Bans a user.")
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, user: discord.Option(discord.Member, description="User to ban", required=True), 
                   reason: discord.Option(str, description="Reason for ban", required=True)):
@@ -35,7 +39,7 @@ class Moderation(commands.Cog):
 
       await ctx.respond(embed=embed)
     
-    @commands.slash_command(name="kick", description="Kicks a user.")
+    @group.command(name="kick", description="Kicks a user.")
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, user: discord.Option(discord.Member, description="User to kick", required=True), 
                   reason: discord.Option(str, description="Reason for ban", required=True)):
@@ -49,7 +53,7 @@ class Moderation(commands.Cog):
 
       await ctx.respond(embed=embed)
 
-    @commands.slash_command(name="poll", description="Creates a votable yes or no poll!")
+    @group.command(name="poll", description="Creates a votable yes or no poll!")
     @commands.has_permissions(administrator = True)
     async def poll(self, ctx, title: discord.Option(str, description="Title of poll", required=True), description: discord.Option(str, description="Your yes or no poll description", required=True)):
        embed = discord.Embed(
@@ -67,7 +71,7 @@ class Moderation(commands.Cog):
        await message.add_reaction(emoji)
        await message.add_reaction(emoji2)
     
-    @commands.slash_command(name="purge", description="Purges a certain number of messages from a channel")
+    @group.command(name="purge", description="Purges a certain number of messages from a channel")
     @commands.has_permissions(manage_messages = True)
     @commands.has_permissions(read_message_history = True)
     async def purge(self, ctx, number: discord.Option(int, description="Number of messages to purge. Max is 100", required=True)):
@@ -75,7 +79,7 @@ class Moderation(commands.Cog):
        await ctx.respond("**" + str(number) + "** messages have been purged!")
 
 
-    @commands.slash_command(name="timeout", description="Timeout a user.")
+    @group.command(name="timeout", description="Timeout a user.")
     @commands.has_permissions(moderate_members=True)
     async def timeout(self, ctx, time: discord.Option(int, description="Amount of minutes to time out user", required=True), user: discord.Option(discord.Member, description="User to timeout", required=True)):
         await user.timeout_for(datetime.timedelta(minutes=time))
@@ -83,24 +87,33 @@ class Moderation(commands.Cog):
 
 
 
-    @commands.slash_command(name="untimeout", description="Removes a timeout from a user.")
+    @group.command(name="untimeout", description="Removes a timeout from a user.")
     @commands.has_permissions(moderate_members=True)
     async def untimeout(self, ctx, user: discord.Option(discord.Member, description="User to untimeout", required=True)):
         await user.remove_timeout()
         await ctx.respond("{0} has had their timeout removed!".format(user))
 
 
-    @commands.slash_command(name="createchannel", description="Creates a basic text channel")
+    @group.command(name="createchannel", description="Creates a basic text channel")
     @commands.has_permissions(manage_channels=True)
     async def createchannel(self, ctx, name: discord.Option(str, description="Name of channel"), guild: discord.Option(discord.Guild, description="Name of server to make channel in. Case sensitive!")):
         await guild.create_text_channel('{0}'.format(name))
         await ctx.respond("The channel **#{0}** has been created!".format(name))
 
-    @commands.slash_command(name="createvoicechannel", description="Creates a basic voice channel")
+    @group.command(name="createvoicechannel", description="Creates a basic voice channel")
     @commands.has_permissions(manage_channels=True)
     async def createchannel(self, ctx, name: discord.Option(str, description="Name of channel"), guild: discord.Option(discord.Guild, description="Name of server to make channel in. Case sensitive!")):
         await guild.create_voice_channel('{0}'.format(name))
         await ctx.respond("The channel **#{0}** has been created!".format(name))
+
+
+    @group.command(name="thread", description="Create a new basic thread")
+    @commands.has_permissions(create_public_threads=True)
+    async def thread(self, ctx, title: discord.Option(str, description="Title of thread", required=True), startmsg: discord.Option(str, description="Starting message in thread", required=True)):
+        message = await ctx.send(startmsg)
+        await message.create_thread(name=title)
+        await ctx.respond("Your thread, **{0}**, has been created!".format(title))
+
 
 
     
@@ -120,5 +133,7 @@ class Moderation(commands.Cog):
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Moderation(bot)) # add the cog to the bot
+
+
 
 
