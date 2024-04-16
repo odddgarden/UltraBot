d3339c096600a585cad9005e777826a0ffe53d9e
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-15 19:40:12 -0700

    Update role.py

--- cogs/role.py
+++ cogs/role.py
@@ -4,7 +4,11 @@ import os
 from discord import reaction
 from discord import Reaction
 
+
+
 class role(commands.Cog):
+    group = discord.SlashCommandGroup(name="moderation", description="Commands for server management and moderation")
+
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
@@ -13,19 +17,19 @@ class role(commands.Cog):
 
 
 
-    @commands.slash_command(name="addrole", description="Adds a role to a user.")
+    @group.command(name="addrole", description="Adds a role to a user.")
     @commands.has_permissions(manage_roles=True)
     async def addrole(self, ctx, user: discord.Option(discord.Member, description="User to give role to", required=True), role: discord.Option(discord.Role, description="Role to give user", required=True)):
        await user.add_roles(role, atomic=True)
        await ctx.respond("The role has been added to the user!")
 
-    @commands.slash_command(name="removerole", description="Removes a role from a user.")
+    @group.command(name="removerole", description="Removes a role from a user.")
     @commands.has_permissions(manage_roles=True)
     async def removerole(self, ctx, user: discord.Option(discord.Member, description="User to remove role from", required=True), role: discord.Option(discord.Role, description="Role to remove", required=True)):
        await user.remove_roles(role, atomic=True)
        await ctx.respond("The role has been removed from the user!")
 
-    @commands.slash_command(name="createrole", description="Creates a basic no perms role.")
+    @group.command(name="createrole", description="Creates a basic no perms role.")
     @commands.has_permissions(manage_roles=True)
     async def createrole(self, ctx, name: discord.Option(str, description="Name of role", required=True), server: discord.Option(discord.Guild, description="Name of the server to make role in. Case sensitive!", required=True)):
        await server.create_role(name=name)
