46411aee3f644b755efd761f48566f1a7213f6bd
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-15 19:36:23 -0700

    Update apis.py

--- cogs/apis.py
+++ cogs/apis.py
@@ -20,16 +20,17 @@ with open("version.json", "r") as f:
 
 
 class Apis(commands.Cog):
+    group = discord.SlashCommandGroup(name="api", description="Commands that use online APIs made by other people")
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
-    @commands.slash_command(name="dadjoke", description="Get a random dad joke!")
+    @group.command(name="dadjoke", description="Get a random dad joke!")
     async def dadjoke(self, ctx):
         r = requests.get("https://official-joke-api.appspot.com/random_joke")
         j = json.loads(r.text)
         await ctx.respond("{0} {1}".format(j["setup"], j["punchline"]))
     
-    @commands.slash_command(name="xkcd", description="Get a random XKCD comic!")
+    @group.command(name="xkcd", description="Get a random XKCD comic!")
     async def xkcd(self, ctx):
         xkcdlink = requests.get("https://xkcd.com/" + str(random.randint(1, 2916)) + "/info.0.json")
         xkcdjson = json.loads(xkcdlink.text)
@@ -47,7 +48,7 @@ class Apis(commands.Cog):
         embed.set_footer(text="Year: " + str(xkcdjson["year"]) + ", Month " + str(xkcdjson["month"]) + ", Day " + str(xkcdjson["day"]))
         await ctx.respond(embed=embed)
     
-    @commands.slash_command(name="dogpics", description="Random picture of a dog!")
+    @group.command(name="dogpics", description="Random picture of a dog!")
     async def dogpics(self, ctx):
         doglink = requests.get("https://dog.ceo/api/breeds/image/random")
         dogjson = json.loads(doglink.text)
@@ -60,7 +61,7 @@ class Apis(commands.Cog):
         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
         await ctx.respond(embed=embed)
     
-    @commands.slash_command(name="shakespeare", description="Translate english text to Shakespeare english!")
+    @group.command(name="shakespeare", description="Translate english text to Shakespeare english!")
     async def shakespeare(self, ctx, text: discord.Option(str, description="Text to translate", required=True)):
          rshake = requests.get("https://api.funtranslations.com/translate/shakespeare.json?text={0}".format(text))
          jshake = json.loads(rshake.text)
