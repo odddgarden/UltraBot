235e74958b78124b5369988318daeeedaa2485da
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-16 13:12:37 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,116 +1,58 @@
 import discord
 from discord.ext import commands
 import os
-import time
+from discord import default_permissions
+from discord import permissions
+from discord import Permissions
+from discord import PermissionOverwrite
+import cowsay
+import nltk
+import random
 import json
+nltk.download('words')
+
+word_list = ["Heads", "Tails"]
+eightball_list = ["It is certain.", "It is decidedly so", "Without a doubt", "Yes - definitely", "You may rely on it", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+random_word = random.choice(word_list)
+random_8ball = random.choice(eightball_list)
 
 with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
 
 
-class fun(commands.Cog):
-    group = discord.SlashCommandGroup(name="fun", description="Fun commands for your enjoyment")
+class Fun(commands.Cog):
+    group = discord.SlashCommandGroup(name="fun", description="Commands made for having fun!")
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
+
+    @group.command(name="cowsay", description="The cowsay command from Linux!")
+    async def cowsay(self, ctx, text=discord.Option(str, description="Text for cow to say", required=True), 
+                     character=discord.Option(str, description="Character for cowsay to use."), 
+                     choices=cowsay.char_names, default="cow"):
+        await ctx.respond("```\n{0}\n```".format(cowsay.get_output_string(character, text))),
     
-    @group.command(name="avatar", description="Find the avatar of the mentioned user!")
-    async def avatar(self, ctx, user: discord.Option(discord.Member, description="Member to get avatar of", required=True)):
-      await ctx.respond(user.display_avatar)
+    @group.command(name="randomword", description="Gives you a random English word!")
+    async def randomword(self, ctx):
+        await ctx.respond("Your Random Word: `" + random.choice(nltk.corpus.words.words()) + "`")
+    
+    @group.command(name="coinflip", description="Flip a coin!")
+    async def coinflip(self, ctx):
+        await ctx.respond(":coin: **" + random_word + "**")
 
-
-    @group.command(name="makeembed", description="Make your own embed!")
-    async def makeembed(self, ctx, title: discord.Option(str, description="Title of embed"), description: discord.Option(str, description="Description of embed"), footer: discord.Option(str, description="Footer of embed"), color: discord.Option(int, description="Color of embed in hex format")):
+    @group.command(name="8ball", description="The Magic 8 Ball answers your question!")
+    async def _8ball(self, ctx, text=discord.Option(str, description="Question to ask the 8 ball")):
         embed = discord.Embed(
-            title=title,
-            description=description,
-            color=color,
+            title="Magic 8 Ball: " + text,
+            description=random_8ball,
+            color=discord.Colour.darker_gray(),
+            
         )
-        embed.set_footer(text=footer)
+        embed.set_thumbnail(url="https://icons.iconarchive.com/icons/barkerbaggies/pool-ball/256/Ball-8-icon.png")
+        embed.set_author(name="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
         await ctx.respond(embed=embed)
-
-    
-    @group.command(name="gettime", description="Returns the current date and time.")
-    async def gettime(self, ctx):
-        await ctx.respond(time.ctime)
-
-    @group.command(name="timestop", description="Stop time in a server JJBA style")
-    @commands.has_permissions(manage_channels=True)
-    async def timestop(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        embed = discord.Embed(
-            title="ZA WARUDO!",
-            description="Time has been stopped! No messages can be sent except for admins.",
-            color=discord.Colour.red(),
-        )
-        embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-        embed.set_image(url="https://i.redd.it/05vtn9chak101.gif")
-        await ctx.respond(embed=embed)
-
-    @group.command(name="resume", description="Resumes time in a server")
-    @commands.has_permissions(manage_channels=True)
-    async def resume(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-        await ctx.respond("Time has been resumed!")
-    
-    @group.command(name="userinfo", description="Gets info on a user in the server!")
-    async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True)):
-        if user.bot == True:
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
-             **ID:** {0}
-             **Joined Discord:** {1}
-             **Discriminator:** {2} 
-             **Is A Bot?:** {3} 
-             """.format(user.id, user.created_at, user.discriminator, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url=user.avatar)
-        
-        if user.bot == False:
-    
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
-             **ID:** {0}
-             **Joined Discord:** {1} 
-             **Is A Bot?:** {2} 
-             """.format(user.id, user.created_at, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url=user.avatar)
-        await ctx.respond(embed=embed)
-          
-
-    @group.command(name="botinfo", description="Info about UltraBot!")
-    async def botinfo(self, ctx):
-         embed = discord.Embed(
-              title="Bot Info",
-              description="""
-              **Bot Name:** UltraBot
-              **Bot Owner:** @combinesoldier14
-              **Creation Date:** 4/5/2024
-              **Server Count:** Unavailable
-              **Library**: Py-cord {0}
-              """.format(discord.__version__),
-              color=discord.Colour.og_blurple(),
-         )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
-         await ctx.respond(embed=embed)
-
-
-
-
-
-
-
 
 def setup(bot): # this is called by Pycord to setup the cog
-    bot.add_cog(fun(bot)) # add the cog to the bot
+    bot.add_cog(Fun(bot)) # add the cog to the bot
+
