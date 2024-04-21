fb800afb3c4f1e23be0366278bb15b5f386efa07
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-20 22:55:21 -0700

    Update mbti.py

--- cogs/mbti.py
+++ cogs/mbti.py
@@ -285,109 +285,6 @@ class Mbti(commands.Cog):
          pjstatus = "J"
          pjlong = "Judging"
 
-     funfact = "undefined"
-     if iestatus == "I" and snstatus == "N" and tfstatus == "T" and pjstatus == "J":
-         funfact = "INTJ is the most introverted type, and is great with coming up with plans!"
-         famoustype = "Christopher Nolan and Michelle Obama"
-         famousfiction = "Emperor Palpatine and Giorno Giovanna"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/intj-architect.png"
-
-     if iestatus == "E" and snstatus == "N" and tfstatus == "T" and pjstatus == "J":
-         funfact = "ENTJ is great with planning, and are most likely to be leaders."
-         famoustype = "Steve Jobs and Gordon Ramsey"
-         famousfiction = "Funny Valentine and Ultron"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/entj-commander.png"
-
-     if iestatus == "I" and snstatus == "N" and tfstatus == "T" and pjstatus == "P":
-         funfact = "INTP are the most intelligent type and are likely to be scientists!"
-         famoustype = "Einstein and Bill Gates"
-         famousfiction = "Yoda and L (from death note)"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/intp-logician.png"
-
-     if iestatus == "E" and snstatus == "N" and tfstatus == "T" and pjstatus == "P":
-         funfact = "ENTP is great at debating, and are likely to push their ideas very strongly!"
-         famoustype = "Weird Al and Adam Savage"
-         famousfiction = "Joker and Iron Man"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/entp-debater.png"
-
-     if iestatus == "I" and snstatus == "N" and tfstatus == "F" and pjstatus == "J":
-         funfact = "INFJ are the most intuitive type and are the most extroverted introverts! They tend to be very spiritual."
-         famoustype = "Lady Gaga and Martin Luther King"
-         famousfiction = "Enrico Pucci and Deku"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/infj-advocate.png"
-
-     if iestatus == "I" and snstatus == "N" and tfstatus == "F" and pjstatus == "P":
-         funfact = "INFP are the most popular type for fictional main characters!"
-         famoustype = "JRR Tolkien and Shakespeare"
-         famousfiction = "Luke Skywalker and Will Byers"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/infp-mediator.png"
-
-     if iestatus == "E" and snstatus == "N" and tfstatus == "F" and pjstatus == "J":
-         funfact = "ENFJ are very good and charismatic leaders!"
-         famoustype = "Obama and Oprah Winfrey"
-         famousfiction = "Mike Wheeler and Homelander"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/enfj-protagonist.png"
-         
-
-     if iestatus == "E" and snstatus == "N" and tfstatus == "F" and pjstatus == "P":
-         funfact = "ENFPs are great at spreading their passions, and are the most introverted extroverts!"
-         famoustype = "Robin Williams and Quentin Tarantino"
-         famousfiction = "Michael Scott and Joyce Byers"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/enfp-campaigner.png"
-
-     if iestatus == "I" and snstatus == "S" and tfstatus == "T" and pjstatus == "J":
-         funfact = "ISTJs are very funny, and are very reliable!"
-         famoustype = "Sting and Natalie Portman"
-         famousfiction = "Hermoine Granger and Sheldon Cooper"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/istj-logistician.png"
-
-     if iestatus == "I" and snstatus == "S" and tfstatus == "F" and pjstatus == "J":
-         funfact = "ISFJs are very loving and caring for their loved ones, and are very generous!"
-         famoustype = "Vin Diesel and Beyonce"
-         famousfiction = "Captain America and Marge Simpson"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/isfj-defender.png"
-        
-
-     if iestatus == "E" and snstatus == "S" and tfstatus == "T" and pjstatus == "J":
-         funfact = "ESTJs are great at managing others and care about making a difference!"
-         famoustype = "Judge Judy and Rockefeller"
-         famousfiction = "Franziska von Karma and Mr. Krabs"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/estj-executive.png"
-
-     if iestatus == "E" and snstatus == "S" and tfstatus == "F" and pjstatus == "J":
-         funfact = "ESFJs are very caring and eager to help!"
-         famoustype = "Taylor Swift and Steve Harvey"
-         famousfiction = "Dorothy Gale and Woody"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/esfj-consul.png"
-
-     if iestatus == "I" and snstatus == "S" and tfstatus == "T" and pjstatus == "P":
-         funfact = "ISTPs are creative builders who love a hands-on experience with anything!"
-         famoustype = "Harrison Ford and Michael Jordan"
-         famousfiction = "Jotaro Kujo and Indiana Jones"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/istp-virtuoso.png"
-
-     if iestatus == "I" and snstatus == "S" and tfstatus == "F" and pjstatus == "P":
-         funfact = "ISFPs are creative and expressive, and usually are artists!"
-         famoustype = "Kevin Costner and Jungkook"
-         famousfiction = "Jesse Pinkman and Eleven"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/isfp-adventurer.png"
-
-     if iestatus == "E" and snstatus == "S" and tfstatus == "T" and pjstatus == "P":
-         funfact = "ESTPs are the most extroverted type and are very adventerous!"
-         famoustype = "Ernest Hemingway and Eddie Murphy"
-         famousfiction = "Sonic the Hedgehog and Joseph Joestar"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/estp-entrepreneur.png"
-
-     if iestatus == "E" and snstatus == "S" and tfstatus == "F" and pjstatus == "P":
-         funfact = "ESFPs are the most energetic personality, and are the most emotional!"
-         famoustype = "Elton John and Adele"
-         famousfiction = "Homer Simpson and Peter Griffin"
-         icon = "https://www.16personalities.com/static/images/personality-types/avatars/esfp-entertainer.png"
-
-     
-
-     
-
      
 
 
@@ -406,10 +303,6 @@ class Mbti(commands.Cog):
          """.format(ielong, snlong, tflong, pjlong, str(iperc3), str(eperc3), str(sperc3), str(nperc3), str(tperc3), str(fperc3), str(pperc3), str(jperc3)),
          color=discord.Colour.red()
      )
-     embed.set_thumbnail(url=icon)
-     embed.add_field(name="Fun Fact about {0}{1}{2}{3}s:".format(iestatus, snstatus, tfstatus, pjstatus), value=funfact)
-     embed.add_field(name="Famous {0}{1}{2}{3}s:".format(iestatus, snstatus, tfstatus, pjstatus), value=famoustype)
-     embed.add_field(name="Famous Fictional {0}{1}{2}{3}s:".format(iestatus, snstatus, tfstatus, pjstatus), value=famousfiction)
      embed.set_footer(text="Learn more about this type: https://www.16personalities.com/{0}{1}{2}{3}-personality".format(iestatus.lower(), snstatus.lower(), tfstatus.lower(), pjstatus.lower()))
      await ctx.respond(embed=embed)
 
