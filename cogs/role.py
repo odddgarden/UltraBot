f0f556c9ab51e944441502a9f285be8374470047
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-04-28 18:30:40 -0700

    Update role.py

--- cogs/role.py
+++ cogs/role.py
@@ -35,6 +35,25 @@ class role(commands.Cog):
        await server.create_role(name=name)
        await ctx.respond("The role **" + name + "** has been created.")
 
+    @group.command(name="roleinfo", description="Gets detailed info on a role")
+    async def roleinfo(self, ctx, role: discord.Option(discord.Role, description="Role to get info on")):
+         embed = discord.Embed(
+              title = "Info on {0}".format(role.name),
+              description = """
+              **Created at:** {0} 
+              **Hoisted:** {1} 
+              **Mentionable:** {2}
+              **Position:** {3} 
+              
+              
+              """.format(role.created_at, role.hoist, role.mentionable, str(role.position)),
+              color = role.colour,
+         
+         )
+         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+         
+         await ctx.respond(embed=embed)
+
 
 
 def setup(bot): # this is called by Pycord to setup the cog
