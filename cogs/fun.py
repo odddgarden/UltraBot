b80a4c7f465e74eeb3654c42e1036edceff3c52b
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-05-01 12:43:02 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -13,8 +13,6 @@ nltk.download('words')
 
 word_list = ["Heads", "Tails"]
 eightball_list = ["It is certain.", "It is decidedly so", "Without a doubt", "Yes - definitely", "You may rely on it", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
-random_word = random.choice(word_list)
-
 with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
