b5f9c266633a11d64833682a4b36ce0b8102c260
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-05-01 22:35:20 -0700

    Update mcstatus.py

--- cogs/mcstatus.py
+++ cogs/mcstatus.py
@@ -10,6 +10,22 @@ with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
 
+with open("dev.json", "r") as f:
+            _r = json.load(f)
+            dev_status = _r["DEV_STATUS"]
+
+#The Dev status is meant for if UltraBot is running in DEV mode which changes some names and icons.
+
+if dev_status == "true":
+            name = "UltraBot Development Edition"
+            game = "with unstable ass commands"
+            icon = "https://cdn.discordapp.com/avatars/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=1024"
+
+if dev_status == "false":
+            name = "UltraBot"
+            game = "in the Python CMD"
+            icon = "https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256"
+
 
 
 
@@ -42,7 +58,7 @@ class mcstatus(commands.Cog):
            embed.add_field(name="Version/Protocol", value="{0} (Protocol {1})".format(javaversion, javaprotocol))
            embed.add_field(name="Secure Chat?", value=str(javastatus.enforces_secure_chat))
            embed.set_thumbnail(url="https://static-00.iconduck.com/assets.00/java-icon-1511x2048-6ikx8301.png")
-           embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+           embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
            
            
            await ctx.respond(embed=embed)
@@ -69,7 +85,7 @@ class mcstatus(commands.Cog):
            embed.add_field(name="Latency", value=f"{bedrocklatency} ms")
            embed.add_field(name="Version/Protocol", value="{0} (Protocol {1})".format(bedrockversion, bedrockprotocol))
            embed.set_thumbnail(url="https://gamepedia.cursecdn.com/minecraft_gamepedia/6/68/Bedrock_JE2_BE2.png?version=fe113612ba2231b70dbf6627c699e644")
-           embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+           embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
            
            
            await ctx.respond(embed=embed)
