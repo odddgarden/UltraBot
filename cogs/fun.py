129dbb46961dfbea24391003b1918883fb107317
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-05-01 22:34:27 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -17,6 +17,24 @@ with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
 
+with open("dev.json", "r") as f:
+            _r = json.load(f)
+            dev_status = _r["DEV_STATUS"]
+
+
+#The Dev status is meant for if UltraBot is running in DEV mode which changes some names and icons.
+
+
+if dev_status == "true":
+            name = "UltraBot Development Edition"
+            game = "with unstable ass commands"
+            icon = "https://cdn.discordapp.com/avatars/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=1024"
+
+if dev_status == "false":
+            name = "UltraBot"
+            game = "in the Python CMD"
+            icon = "https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256"
+
 
 class Fun(commands.Cog):
     group = discord.SlashCommandGroup(name="fun", description="Commands made for having fun!")
@@ -47,7 +65,7 @@ class Fun(commands.Cog):
             
         )
         embed.set_thumbnail(url="https://icons.iconarchive.com/icons/barkerbaggies/pool-ball/256/Ball-8-icon.png")
-        embed.set_author(name="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+        embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
         await ctx.respond(embed=embed)
 
 def setup(bot): # this is called by Pycord to setup the cog
