b573e3f71cfddfba82e2d0832097b0c5c9e7cc41
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-05-01 22:36:12 -0700

    Update role.py

--- cogs/role.py
+++ cogs/role.py
@@ -10,6 +10,25 @@ with open("version.json", "r") as f:
             _r = json.load(f)
             VERSION = _r["VERSION"]
 
+with open("dev.json", "r") as f:
+            _r = json.load(f)
+            dev_status = _r["DEV_STATUS"]
+
+
+
+#The Dev status is meant for if UltraBot is running in DEV mode which changes some names and icons.
+
+
+if dev_status == "true":
+            name = "UltraBot Development Edition"
+            game = "with unstable ass commands"
+            icon = "https://cdn.discordapp.com/avatars/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=1024"
+
+if dev_status == "false":
+            name = "UltraBot"
+            game = "in the Python CMD"
+            icon = "https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256"
+
 
 class role(commands.Cog):
     group = discord.SlashCommandGroup(name="role", description="Commands for managing roles")
@@ -55,7 +74,7 @@ class role(commands.Cog):
               color = role.colour,
          
          )
-         embed.set_footer(text="UltraBot " + VERSION, icon_url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
          
          await ctx.respond(embed=embed)
 
