d403f6670a505a0128ab51bf3b85bed8d763f7a2
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-05-02 17:01:07 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -68,7 +68,7 @@ class Fun(commands.Cog):
         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
         await ctx.respond(embed=embed)
 
-   @group.command(name="randomyt", description="Get a random youtube video")
+    @group.command(name="randomyt", description="Get a random youtube video")
     async def randomyt(self, ctx):
          
          listvalues = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
